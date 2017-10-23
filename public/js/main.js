53ee25b4e6c445429620bedb7b3978c554f01543
Skip <skiphog@mail.ru>
2017-10-23 22:19:42 +0300

    add minify

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,1 @@
-window.addEventListener('resize', function (event) {
-
-  if (this.resizeUI) {
-    clearTimeout(this.resizeUI);
-  }
-
-  this.resizeUI = setTimeout(function () {
-    Processing.getInstanceById('canvas').resize();
-  }, 200);
-});
+window.addEventListener("resize",function(event){if(this.resizeUI){clearTimeout(this.resizeUI);}this.resizeUI=setTimeout(function(){Processing.getInstanceById("canvas").resize();},200);});
