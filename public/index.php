5165da36be320c6c9c61afa6a57ad26f928ad254
Skip <skiphog@mail.ru>
2017-10-23 16:24:35 +0300

    add html into index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
-<?php
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/main.css">
+    <title>SkipHog</title>
+</head>
+<body class="body">
 
-echo 'Hello world';
+<header id="header">
+    <h1 class="title">SKIPHOG<br>WEB developer</h1>
+    <a class="link" href="https://skiphog.ru">https://skiphog.ru</a>
+</header>
+
+<canvas id="canvas" class="canvas" data-processing-sources="/js/huddle.pde"></canvas>
+
+<script src="/js/main.js"></script>
+<script src="/js/processing-1.4.1.min.js"></script>
+</body>
+</html>
