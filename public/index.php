3e9cbad75e598d2b04ece3568cc9c4f7a46e53bd
Skip <skiphog@mail.ru>
2017-10-24 18:59:44 +0300

    change favicon

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/img/favicon.png">
     <link rel="stylesheet" href="/css/main.css">
     <title>skiphog</title>
 </head>
